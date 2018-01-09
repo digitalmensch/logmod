43d0a5a5a9622681a535bd501684d7d80b17f53c
TA <tag@adnm.ch>
2018-01-09 01:04:54 +0000

    Implement

--- example.py
+++ example.py
@@ -1,16 +1,12 @@
+import logmod
 import secrets
 import random
-import logmod
 
 logmod.logmod(secrets, random)
 
-def cool_number(x):
-    return random.random() * x * random.randint(0, x)
+def coolname():
+    return secrets.token_hex(5)
 
-for i in range(10):
-    print(i, random.random(), cool_number(i))
-
-
-hex_name = secrets.token_hex(5)
-print(f'Hello, {hex_name}')
-
+name = coolname()
+print(f'Hello, {name}')
+print(random.random())
